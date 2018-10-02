8bed147b9dc3c32872fcd3d17a25c3d734fa3844
Hilary <c011605154@edu.teu.ac.jp>
2018-10-02 11:53:10 +0900

    tkinterでウィンドウ表示

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Kameda")
+import cv2
+import matplotlib
+import numpy
+import tkinter
+
+root = tkinter.Tk()
+#設定
+root.option_add("*font", ["MS Pゴシック", 22])
+
+#ウィンドウ設定
+root.title("KMDさんぐるぐる")
+root.geometry("640x480+100+100")
+
+text1 = tkinter.Label(text="ぐるぐるさせたい", bg="white")
+text1.place(x=10, y=10)
+
+# メインループ
+root.mainloop()
+
